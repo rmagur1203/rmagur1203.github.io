adbc577733eeb56cb1ff38a9ba1068e9b877d27e
rmagur1203 <rmagur1203@naver.com>
2020-06-26 19:44:00 +0900

    AfterPack

---
+++ engword/js/ClickAnswer.js
@@ -0,0 +1,63 @@
+answerCount = $.cookie("score");
+if (answerCount == undefined) answerCount = 0;
+$(document).ready(function () {
+    LoadWord();
+});
+function LoadWord() {
+    $.getJSON("words.json", function(json) {
+        let container = document.getElementById("container");
+        let answers = document.createElement("div");
+        answers.id = "answers";
+        data = json[getRandomInt(1, 3001)];
+        keys = shuffle([
+            json[getRandomInt(1, 3001)],
+            json[getRandomInt(1, 3001)],
+            json[getRandomInt(1, 3001)],
+            json[getRandomInt(1, 3001)],
+            data
+        ]);
+        keys.push({뜻:"모릅니다."});
+        container.innerHTML = `<div id="wordCard" data-toggle="tooltip" title="[${data.등급}] ${data.뜻}">${data.단어}</div>`;
+        container.appendChild(answers);
+        let answerhtml = "";
+        answerhtml = "<center>";
+        for (let i = 0;i < keys.length;i++){
+            answerhtml += "<br/>";
+            answerhtml += "<br/>";
+            answerhtml += `<a class="button" id="${i}" onclick="checkAnswer(this);">${keys[i].뜻}</a>`;      
+        }
+        answers.innerHTML = answerhtml + "</center>";
+        container.innerHTML += `<div id="Correct">${answerCount}</div>`;
+    });
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+function checkAnswer(sender){
+    if (sender.classList.contains("disable")) return;
+    let id = sender.id;
+    console.log(id);
+    if (data == keys[id]) {
+        answerCount++;
+        $.cookie("score", answerCount);
+    }
+    $('[data-toggle="tooltip"]').tooltip("show");
+    let container = document.getElementById("container");
+    container.style.backgroundColor = (data == keys[id] ? "#28a745" : "#dc3545");
+    $("a.button").addClass("disable");
+    setTimeout(() => {
+        let container = document.getElementById("container");
+        container.style.backgroundColor = "";
+        $('[data-toggle="tooltip"]').tooltip("hide");
+        LoadWord();
+    }, 2000);
+}

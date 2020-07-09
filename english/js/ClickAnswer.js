5163da65d2ed69ba51a3beb82514a29573f96bfe
rmagur1203 <rmagur1203@naver.com>
2020-07-09 20:08:11 +0900

    Update speech

--- english/js/ClickAnswer.js
+++ english/js/ClickAnswer.js
@@ -1,8 +1,9 @@
 answerCount = $.cookie("score");
 if (answerCount == undefined) answerCount = 0;
-$(document).ready(function () {
+$(document).ready(function() {
     LoadWord();
 });
+
 function LoadWord() {
     $.getJSON("words.json", function(json) {
         let container = document.getElementById("container");
@@ -16,25 +17,28 @@ function LoadWord() {
             json[getRandomInt(1, 3001)],
             data
         ]);
-        keys.push({뜻:"모릅니다."});
+        keys.push({ 뜻: "모릅니다." });
         container.innerHTML = `<div id="wordCard" data-toggle="tooltip" title="[${data.등급}] ${data.뜻}">${data.단어}</div>`;
         container.appendChild(answers);
         let answerhtml = "";
         answerhtml = "<center>";
-        for (let i = 0;i < keys.length;i++){
+        for (let i = 0; i < keys.length; i++) {
             answerhtml += "<br/>";
             answerhtml += "<br/>";
-            answerhtml += `<a class="button" id="${i}" onclick="checkAnswer(this);">${keys[i].뜻}</a>`;      
+            answerhtml += `<a class="button" id="${i}" onclick="checkAnswer(this);">${keys[i].뜻}</a>`;
         }
         answers.innerHTML = answerhtml + "</center>";
         container.innerHTML += `<div id="Correct">${answerCount}</div>`;
+        speech(data.단어, { lang: "en-US" });
     });
 }
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -42,14 +46,18 @@ function shuffle(a) {
     }
     return a;
 }
-function checkAnswer(sender){
+
+function checkAnswer(sender) {
     if (sender.classList.contains("disable")) return;
     let id = sender.id;
     console.log(id);
     if (data == keys[id]) {
         answerCount++;
         $.cookie("score", answerCount);
+    } else {
+        answerCount = 0;
     }
+    speech(data.뜻);
     $('[data-toggle="tooltip"]').tooltip("show");
     let container = document.getElementById("container");
     container.style.backgroundColor = (data == keys[id] ? "#28a745" : "#dc3545");
